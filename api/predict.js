467bed6a1028f52f0454d143e15404589ce7d83c
faceevol <peiyalu@gmail.com>
2026-08-23 12:04:03 +0200

    Update predict.js

--- api/predict.js
+++ api/predict.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  if (req.method !== "POST") {
+  if (!["GET", "POST"].includes(req.method)) {
     return res.status(405).json({
       error: "Method not allowed"
     });
